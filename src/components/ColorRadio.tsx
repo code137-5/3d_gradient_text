18987c7e77ba948d0480f7c81bbbc85ca9101c8d
Hadong Lee <hadongme@gmail.com>
2024-07-18 16:38:40 +0900

    update - 칼라 팔레트 업데이트

--- src/components/ColorRadio.tsx
+++ src/components/ColorRadio.tsx
@@ -7,6 +7,13 @@ interface ColorRadioProps {
   onPaletteChange: (palette: string) => void;
 }
 
+const radioStyle = (backgroundColor: string) => ({
+  color: 'white',
+  fontWeight: 'bold',
+  backgroundColor,
+  opacity: 0.8,
+});
+
 const ColorRadio: React.FC<ColorRadioProps> = ({ selectedPalette, onPaletteChange }) => {
   const handlePaletteChange = (e: RadioChangeEvent) => {
     onPaletteChange(e.target.value);
@@ -15,15 +22,15 @@ const ColorRadio: React.FC<ColorRadioProps> = ({ selectedPalette, onPaletteChang
   return (
     <div className="color-radio">
       <Radio.Group value={selectedPalette} onChange={handlePaletteChange}>
-        <Radio.Button value="lesbian">L</Radio.Button>
-        <Radio.Button value="gay">G</Radio.Button>
-        <Radio.Button value="bisexual">B</Radio.Button>
-        <Radio.Button value="transgender">T</Radio.Button>
-        <Radio.Button value="nonbinary">NB</Radio.Button>
-        <Radio.Button value="asexual">A</Radio.Button>
-        <Radio.Button value="intersex">I</Radio.Button>
-        <Radio.Button value="queer">Q</Radio.Button>
-        <Radio.Button value="genderfluid">GF</Radio.Button>
+      <Radio.Button value="lesbian" style={radioStyle('#d362a4')}>Lesbian</Radio.Button>
+      <Radio.Button value="gay" style={radioStyle('#1ca3ec')}>Gay</Radio.Button>
+      <Radio.Button value="bisexual" style={radioStyle('#d60270')}>Bisexual</Radio.Button>
+      <Radio.Button value="transgender" style={radioStyle('#55cdfc')}>Transgender</Radio.Button>
+      <Radio.Button value="nonbinary" style={radioStyle('#000')}>Nonbinary</Radio.Button>
+      <Radio.Button value="asexual" style={radioStyle('#a4a4a4')}>Asexual</Radio.Button>
+      <Radio.Button value="intersex" style={radioStyle('#ffda00')}>Intersex</Radio.Button>
+      <Radio.Button value="queer" style={radioStyle('#b57edc')}>Queer</Radio.Button>
+      <Radio.Button value="genderfluid" style={radioStyle('#ff75a2')}>Genderfluid</Radio.Button>
       </Radio.Group>
     </div>
   );
